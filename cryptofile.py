391c025ef9d6eb32fde755e4452a04bee347b508
Caio Madeira <51931284+CaioMadeira@users.noreply.github.com>
2021-02-23 11:49:40 -0300

    Using tkinter

--- cryptofile.py
+++ cryptofile.py
@@ -1,14 +1,98 @@
 from functionalities import Functionalities
+from tkinter import *
+from tkinter import ttk
+import dotenv
+import os
+
+dotenv.find_dotenv()
+dotenv.load_dotenv()
+
 
 class MainApplication(Functionalities):
 
     def __init__(self):
-        self.key = int(input("Escreva uma chave de 1 á 255:"))
-        self.filename = input("Digite o nome do arquivo:")
-        self.remove_extension(filename=self.filename)
-        #self.encrypt_file(filename=self.filename, key=self.key)
-        self.decrypt_file(filename=self.filename, key=self.key)
+        super().__init__()
+        self.folder_name_choose = ""
+        self.folder_name_save = ""
+        self.root = Tk()
+        self.screen_config()
+        self.key = 0
+        self.widgets()
+
+        # self.screen()
+
+    def screen_config(self):
+        self.root.title(os.getenv('screen_title'))
+        self.root.geometry(os.getenv('screen_size'))
+        self.root.columnconfigure(0, weight=1)
+        self.root.resizable = False
+
+    def widgets(self):
+        first_label = Label(self.root, text=os.getenv('text_label_0'),
+                            font=(os.getenv('font_2'), os.getenv('smallsize_2')))
+        first_label.grid()
+
+        key_label = Label(self.root, text=os.getenv('text_label_8'),
+                          font=os.getenv('font_2', os.getenv('smallsize_1')))
+        key_label.grid()
+
+        # self.key_entry_var = IntVar()
+        self.key_entry = Entry(self.root, width=30)
+        self.key_entry.grid()
+
+        # ESCOLHER CAMINHO P/ ABRIR ==========================
+        choose_path_label = Label(self.root, text=os.getenv('text_label_1'),
+                                  font=os.getenv('font_2', os.getenv('smallsize_1')))
+        choose_path_label.grid()
+
+        self.select_path_entry = Entry(self.root, width=50)
+        self.select_path_entry.grid()
+
+        self.error_location = Label(self.root, text=os.getenv('text_label_2'), fg=os.getenv('red'),
+                                    font=os.getenv('font_2'))
+        self.error_location.grid()
+
+        choose_path_button = Button(self.root, width=os.getenv('small_size_button'), bg=os.getenv('red'),
+                                    fg=os.getenv('white'),
+                                    text=os.getenv('text_button_0'), command=self.open_file_path_to_choice)
+        choose_path_button.grid()
+        # =====================================================
+        # ESCOLHER CAMINHO P/ SALVAR ==========================
+
+        choose_path_label_2 = Label(self.root, text=os.getenv('text_label_7'),
+                                    font=os.getenv('font_2', os.getenv('smallsize_1')))
+        choose_path_label_2.grid()
+
+        self.select_path_entry_2 = Entry(self.root, width=50)
+        self.select_path_entry_2.grid()
+
+        self.error_location_2 = Label(self.root, text=os.getenv('text_label_2'), fg=os.getenv('red'),
+                                      font=os.getenv('font_2'))
+        self.error_location_2.grid()
+
+        choose_path_button_2 = Button(self.root, width=os.getenv('small_size_button'), bg=os.getenv('red'),
+                                      fg=os.getenv('white'),
+                                      text=os.getenv('text_button_0'), command=self.open_file_path_to_save)
+        choose_path_button_2.grid()
+        # =====================================================
+        # ESCOLHER A HASH E ENCRYPT ==========================
+        choose_hash_label = Label(self.root, text=os.getenv('text_label_5'), font=os.getenv('smallsize_2'))
+        choose_hash_label.grid()
+
+        choice_hash_box = ttk.Combobox(self.root, values=os.getenv('choice_hash'))
+        choice_hash_box.grid()
+
+        encrypt_button = Button(self.root, text=os.getenv('text_button_1'), width=10, bg=os.getenv('red'),
+                                fg=os.getenv('white'), command=self.encrypt_file)
+        encrypt_button.grid()
+        # =====================================================
+        # CREDITS ==========================
+
+        credits_label = Label(self.root, text=os.getenv('text_credits'),
+                              font=os.getenv('arial', os.getenv('smallsize_1')))
+        credits_label.grid()
 
 
 if __name__ == '__main__':
-    MainApplication()
+    main = MainApplication()
+    mainloop()
--- functionalities.py
+++ functionalities.py
@@ -1,46 +1,71 @@
-import os
 import hashlib
 import time
+from shutil import copyfile
+from tkinter import filedialog
+import os
 
 
 class Functionalities:
 
-    def remove_extension(self, filename):
-        self.extract_filename, self.extract_extension = os.path.splitext(filename)
+    def remove_extension(self):
+        self.extract_filename, self.extract_extension = os.path.splitext(self.filename)
 
-    def encrypt_file(self, filename, key):
-        file = open(filename, "rb")
+    def encrypt_file(self):
+        file = open(self.filename, "rb")
         data = file.read()
         self.hash_filename = hashlib.md5(data).hexdigest()
         file.close()
 
         data = bytearray(data)
         for index, value in enumerate(data):
-            data[index] = value ^ key
+            data[index] = value ^ self.key
 
         file = open(f'{self.hash_filename}-{self.extract_filename}{self.extract_extension}', "wb")
         file.write(data)
         file.close()
+        self.move_file_to_path_save()
 
-    def decrypt_file(self, filename, key):
-        file = open(filename, "rb")
+    def decrypt_file(self):
+        file = open(self.filename, "rb")
         data = file.read()
         file.close()
 
         data = bytearray(data)
         for index, value in enumerate(data):
-            data[index] = value ^ key
+            data[index] = value ^ self.key
 
-        file = open(filename, "wb")
+        file = open(self.filename, "wb")
         file.write(data)
         file.close()
-        separate_hash = filename[0:33]
-        file_2 = filename.replace(f'{separate_hash}', '')
+        separate_hash = self.filename[0:33]
+        file_2 = self.filename.replace(f'{separate_hash}', '')
         try:
             time.sleep(1)
             os.remove(file_2)
         except:
             pass
 
-        os.rename(filename, file_2)
+        os.rename(self.filename, file_2)
+
+
+    def open_file_path_to_choice(self):
+        self.folder_name_choose = filedialog.askdirectory()
+        if (len(self.folder_name_choose) > 1):
+            self.error_location.config(text=self.folder_name_choose, fg=os.getenv('green'))
+
+        else:
+            self.error_location.config(text=os.getenv('text_label_6'), fg=os.getenv('red'))
+
+    def open_file_path_to_save(self):
+        self.folder_name_save = filedialog.askdirectory()
+        if (len(self.folder_name_save) > 1):
+            self.error_location_2.config(text=self.folder_name_save, fg=os.getenv('green'))
+
+        else:
+            self.error_location_2.config(text=os.getenv('text_label_6'), fg=os.getenv('red'))
+
+    def move_file_to_path_save(self):
+        time.sleep(3)
+        copyfile(self.folder_name_choose, self.folder_name_save)
+
 
